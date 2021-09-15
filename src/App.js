65756f210a98e4a0d2faab394573a10ff675edf5
Irina Murashka <irvil@tut.by>
2021-09-15 11:08:19 +0100

    добавила методы жизненного цикла

--- src/App.js
+++ src/App.js
@@ -16,6 +16,24 @@ class App extends Component {
     filter: '',
   };
 
+  
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
   formSubmitHandler = contact => {
     const { contacts } = this.state;
     const { name } = contact;
